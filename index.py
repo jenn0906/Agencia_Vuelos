365ac42f59698b0fd3999951556ad92432742642
jenn0906 <jen.1995.jq@gmail.com>
2024-10-14 16:57:01 -0500

    Plantilla agencia de vuelos

---
+++ index.py
@@ -0,0 +1,13 @@
+
+listaVuelos = []
+idVuelos = 1 
+
+while True :
+    print("---\n Bienvenido a la agencia de vuelos islas tiqui tiqui----")
+    print("1. ¿A dónde quieres ir?")
+    print("2. Leer todas las tarea")
+    print("3. Actualizar una tarea")
+    print("4. Eliminar una tarea")
+    print("5. salir")
+
+    opcion = input("Elige la opcion deseada: ")
