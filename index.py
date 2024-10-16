f90466c5e49d775c28c63f7c0ee7395c7b3de2ed
Manuela0523 <mapibo0706@gmail.com>
2024-10-15 22:53:47 -0500

    Subi paquetes de vuelo

--- index.py
+++ index.py
@@ -77,6 +77,39 @@ while True :
     # else:
     #      print("opcion invalida por favor verifique")       
 
+#-----------------------------------PAQUETES TURISTICOS ------------------------------------
+
+Paquete1 = {
+    "nombre" : "TourMedellin",
+    "duracion" : "cuatro dias y tres noches",
+    "transporte" : "privado",
+    "actividades" : "jardin botanico, parque explora, museo de arte moderno, comuna 13",
+    "seguridad" : "seguro contra accidentes y asistencia medica",
+    "precio": 360.000
+}
+
+Paquete2 = {
+    "nombre" : "MedalloExtremo",
+    "duracion" : "tres dias y dos noches",
+    "transporte" : "privado",
+    "actividades" : "parapente en guatape, visita a las cascadas de san luis, recorrido en cuatrimotos ",
+    "seguridad" : " seguro contra accidentes y asistencia medica",
+    "precio" : 650.000
+}
+
+#---------------------------------------------------------------------------------
+
+Paquete3 = {
+    "nombre" : "CartagenaBeach",
+    "duracion" : "12 horas",
+    "transporte" : "terrestre privado, lancha rapida",
+    "actividades" : "excurción Bora bora beach, isabella y Pao Pao beach.",
+    "alimentación": "coctel de binvenida, almuerzo, refrigerio, agua y gaseosa en la lancha",
+    "seguridad" : "seguro contra accidentes y asistencia medica",
+    "precio": 300.000
+}
+
+
 
 
 
